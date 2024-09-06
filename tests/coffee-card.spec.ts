e92c0492c58603cd408e7d2d5ee90cda494c650e
karina-krupodior <karinakrupodior@gmail.com>
2024-09-05 19:42:22 -0500

    refactoring - added consts

--- tests/coffee-card.spec.ts
+++ tests/coffee-card.spec.ts
@@ -1,44 +1,52 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Locator } from "@playwright/test";
 
 test.describe("Coffee Card Functionality Test", () => {
   test.beforeEach(async ({ page }) => {
-    await page.goto("https://coffee-cart.app/");
+    const ulrForCoffeeCartApp = "https://coffee-cart.app/";
+    await page.goto(ulrForCoffeeCartApp);
   });
 
   test.afterEach(async ({ page }) => {
     await page.close();
   });
   test("payment details form/window should be Visible", async ({ page }) => {
-    await page.locator('[data-test="Espresso"]').click();
-    await page.locator('[data-test="Americano"]').click();
-    await page.locator('[aria-label="Cart page"]').click();
-    await page.locator('[data-test="checkout"]').click();
+    const espressoCup: Locator = page.locator('[data-test="Espresso"]');
+    const americanoCup: Locator = page.locator('[data-test="Americano"]');
+    const cardPage: Locator = page.locator('[aria-label="Cart page"]');
+    const checkout: Locator = page.locator('[data-test="checkout"]');
+    const paymentDetails: Locator = page.locator("h1[data-v-29c3be1b]");
+    const paymentLink: Locator = page.locator("p[data-v-29c3be1b]");
 
-    await page.locator("h1[data-v-29c3be1b]").click();
+    await espressoCup.click();
+    await americanoCup.click();
+    await cardPage.click();
+    await checkout.click();
+    await paymentDetails.click();
+
     const itIsVisible = await page
       .locator('h1:has-text("Payment details")')
       .isVisible();
     expect(itIsVisible).toBe(true);
-    await expect(page.locator("h1[data-v-29c3be1b]")).toContainText(
-      "Payment details"
-    );
-    const IsVisible = await page.locator("p[data-v-29c3be1b]").isVisible();
+
+    await expect(paymentDetails).toContainText("Payment details");
+
+    const IsVisible = await paymentLink.isVisible();
     expect(IsVisible).toBe(true);
 
-    await expect(page.locator("p[data-v-29c3be1b]")).toContainText(
+    await expect(paymentLink).toContainText(
       "We will send you a payment link via email."
     );
   });
 
   test("add item to card", async ({ page }) => {
-    await page.locator('[data-test="Espresso"]').click();
+    const espressoCup: Locator = page.locator('[data-test="Espresso"]');
+    const cardPage: Locator = page.locator('[aria-label="Cart page"]');
 
-    await page.locator('[aria-label="Cart page"]').click();
+    await espressoCup.click();
+    await cardPage.click();
 
-    await expect(page.locator('[aria-label="Cart page"]')).toBeVisible();
-    await expect(page.locator('[aria-label="Cart page"]')).toContainText(
-      "cart (1)"
-    );
+    await expect(cardPage).toBeVisible();
+    await expect(cardPage).toContainText("cart (1)");
 
     const isVisible = await page
       .locator('li.list-item[data-v-8965af83] div:has-text("Espresso")')
@@ -47,76 +55,96 @@ test.describe("Coffee Card Functionality Test", () => {
   });
 
   test("navigate to github page", async ({ page }) => {
-    await page.locator('[aria-label="GitHub page"]').click();
+    const gitHubPage: Locator = page.locator('[aria-label="GitHub page"]');
+    const cardPage: Locator = page.locator('[aria-label="Cart page"]');
+    const gitHubCoffeeCartApp: Locator = page.locator(
+      'a[href="https://github.com/jecfish/coffee-cart"]'
+    );
 
-    await expect(page.locator('[aria-label="Cart page"]')).toBeVisible();
-    await expect(
-      page.locator(
-        '.container[data-v-09b4348a] p:has-text("Star our repository")'
-      )
-    ).toBeVisible();
+    const repository: Locator = page.locator(
+      '.container[data-v-09b4348a] p:has-text("Star our repository")'
+    );
+    await gitHubPage.click();
+    await expect(repository).toBeVisible();
 
-    await page
-      .locator('a[href="https://github.com/jecfish/coffee-cart"]')
-      .click();
+    await expect(cardPage).toBeVisible();
 
-    await expect(
-      page.locator('[aria-label="commits by jecfish"]')
-    ).toBeVisible();
+    await gitHubCoffeeCartApp.click();
+    const elementInRepository: Locator = page.locator(
+      '[aria-label="commits by jecfish"]'
+    );
+    await expect(elementInRepository).toBeVisible();
   });
 
   test("should update card count when item added", async ({ page }) => {
-    await page.locator('[data-test="Americano"]').click();
-    await page.locator("li").filter({ hasText: "cart (1)" }).click();
-    await expect(
-      page.locator("li").filter({ hasText: "cart (1)" })
-    ).toBeVisible();
+    const americanoCup:Locator = page.locator('[data-test="Americano"]');
+    const countCups:Locator = page.locator("li").filter({ hasText: "cart (1)" });
+
+    await americanoCup.click();
+    await countCups.click();
+
+    await expect(countCups).toBeVisible();
     await expect(page.locator("#app")).toContainText("cart (1)");
   });
 
   test("should display empty card message when card empty", async ({
     page,
   }) => {
-    await page.locator('[data-test="Cafe_Latte"]').click();
-    await page.locator('[aria-label="Cart page"]').click();
+    const cafeLatte: Locator = page.locator('[data-test="Cafe_Latte"]');
+    const cardPage: Locator = page.locator('[aria-label="Cart page"]');
+    const deleteCoffeeFormCard: Locator = page.locator(".delete");
+    const emptyCardMessage:Locator = page.locator(".list p[data-v-8965af83]");
+    await cafeLatte.click();
+    await cardPage.click();
 
-    await page.locator(".delete").click();
-    await expect(page.locator(".list p[data-v-8965af83]")).toBeVisible();
-    await expect(page.locator(".list p[data-v-8965af83]")).toContainText(
-      "No coffee, go add some."
-    );
+    await deleteCoffeeFormCard.click();
+
+    await expect(emptyCardMessage).toBeVisible();
+    await expect(emptyCardMessage).toContainText("No coffee, go add some.");
   });
 
   test("id1 - it should show promotion message", async ({ page }) => {
-    await page.locator('[data-test="Espresso"]').click();
-    await page.locator('[data-test="Espresso_Macchiato"]').click();
-    await page.locator('[data-test="Cappuccino"]').click();
-    await expect(page.locator(".yes")).toBeVisible();
+    const EspressoCup: Locator = page.locator('[data-test="Espresso"]');
+    const EspressoMacchiatoCup: Locator = page.locator(
+      '[data-test="Espresso_Macchiato"]'
+    );
+    const CappuccinoCup: Locator = page.locator('[data-test="Cappuccino"]');
+    const confirmationToAddCup: Locator = page.locator(".yes");
+    const skipButton: Locator = page.locator(
+      'button[data-v-852f3b25]:has-text("Nah, I\'ll skip.")'
+    );
+    const promoMessage: Locator = page.locator(".promo span");
 
-    await expect(
-      page.locator('button[data-v-852f3b25]:has-text("Nah, I\'ll skip.")')
-    ).toBeVisible();
+    await EspressoCup.click();
+    await EspressoMacchiatoCup.click();
+    await CappuccinoCup.click();
 
-    await expect(page.locator(".promo span")).toContainText(
+    await expect(confirmationToAddCup).toBeVisible();
+    await expect(skipButton).toBeVisible();
+    await expect(promoMessage).toContainText(
       "It's your lucky day! Get an extra cup of Mocha for $4."
     );
   });
 
   test("id2", async ({ page }) => {
-    await page.locator('[data-test="checkout"]').click();
+    const checkout: Locator = page.locator('[data-test="checkout"]');
+    const name: Locator = page.locator('[name="name"]');
+    const email: Locator = page.locator('[name="email"]');
+    const checkbox: Locator = page.locator('[type="checkbox"]');
+    const submitPayment: Locator = page.locator("#submit-payment");
+    const successMessage:Locator = page.locator('[class="snackbar success"]');
 
-    await page.locator('[name="name"]').click();
-    await page.locator('[name="name"]').fill("Karina");
-
-    await page.locator('[name="email"]').click();
-    await page.locator('[name="email"]').fill("test@gmail.com");
-
+    await checkout.click();
+    await name.click();
+    await name.fill("Karina");
+    await email.click();
+    await email.fill("test@gmail.com");
     await page.getByLabel("Promotion message").click();
-    await page.locator('[type="checkbox"]').check();
+    await checkbox.check();
+    await submitPayment.click();
 
-    await page.locator("#submit-payment").click();
-    await expect(page.locator('[class="snackbar success"]')).toBeVisible();
-    await expect(page.locator('[class="snackbar success"]')).toContainText(
+    await expect(successMessage).toBeVisible();
+    await expect(successMessage).toContainText(
       "Thanks for your purchase. Please check your email for payment."
     );
   });
--- tests/playwright-functionality.spec.ts
+++ tests/playwright-functionality.spec.ts
@@ -1,70 +1,89 @@
 import { test, expect } from "@playwright/test";
 
-test("has title", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+test.describe("Playwright Functionality", () => {
+  test.beforeEach(async ({ page }) => {
+    const ulrForCoffeeCartApp = "https://coffee-cart.app/";
+    await page.goto(ulrForCoffeeCartApp);
+  });
 
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  test("has title", async ({ page }) => {
+    await page.goto("https://playwright.dev/");
 
-test("get started link", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
+    await expect(page).toHaveTitle(/Playwright/);
+  });
 
-  await page.locator(".getStarted_Sjon").click();
+  test("get started link", async ({ page }) => {
+    await page.goto("https://playwright.dev/");
+    const getStarted = page.locator(".getStarted_Sjon");
+    const installation = page.locator('h1:has-text("Installation")');
 
-  await expect(page.locator('h1:has-text("Installation")')).toBeVisible();
-});
+    await getStarted.click();
 
-test("checking opening side bar ", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-  await page.locator('a:has-text("Docs")').click();
-  await page.locator('a:has-text("Migration")').click();
-  await page.locator('[href="/docs/protractor"]').hover();
-  await expect(page.locator('[href="/docs/protractor"]')).toBeVisible();
-  await expect(page.locator('a[href="/docs/protractor"]')).toContainText(
-    "Migrating from Protractor"
-  );
-});
+    await expect(installation).toBeVisible();
+  });
 
-test("checking ability to copy text", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-  await page.locator('.navbar__items [href="/docs/intro"]').click();
-  const codeBlock = page
-    .locator("div")
-    .filter({ hasText: "npm init playwright@latest" })
-    .first();
-  const copyButton = codeBlock
-    .locator('button[aria-label="Copy code to clipboard"]')
-    .first();
+  test("checking opening side bar ", async ({ page }) => {
+    await page.goto("https://playwright.dev/");
+    const navbarLinkDocs = page.locator('a:has-text("Docs")');
+    const migrationMenuLink = page.locator('a:has-text("Migration")');
+    const menuLinkMigratingFromProtractor = page.locator(
+      'a[href="/docs/protractor"]'
+    );
 
-  await expect(copyButton).toBeVisible();
-  await copyButton.click();
-});
+    await navbarLinkDocs.click();
+    await migrationMenuLink.click();
+    await menuLinkMigratingFromProtractor.hover();
 
-test("Verify navigation to GitHub page and visibility of repository details", async ({
-  page,
-}) => {
-  await page.goto("https://playwright.dev/");
+    await expect(menuLinkMigratingFromProtractor).toBeVisible();
+    await expect(menuLinkMigratingFromProtractor).toContainText(
+      "Migrating from Protractor"
+    );
+  });
 
-  await page.locator('[aria-label="GitHub repository"]').click();
+  test("checking ability to copy text", async ({ page }) => {
+    await page.goto("https://playwright.dev/");
+    const navbarLinkDocs = page.locator('.navbar__items [href="/docs/intro"]');
+    await navbarLinkDocs.click();
+    const codeBlock = page
+      .locator("div")
+      .filter({ hasText: "npm init playwright@latest" })
+      .first();
+    const copyButton = codeBlock
+      .locator('button[aria-label="Copy code to clipboard"]')
+      .first();
 
-  const [newPage] = await Promise.all([
-    page.waitForEvent("popup"),
-    page.locator('[aria-label="GitHub repository"]').click(),
-  ]);
+    await expect(copyButton).toBeVisible();
+    await copyButton.click();
+  });
 
-  await newPage.waitForLoadState();
-  await expect(
-    newPage.locator("text=microsoft / playwright Public")
-  ).toBeVisible();
-});
+  test("Verify navigation to GitHub page and visibility of repository details", async ({
+    page,
+  }) => {
+    await page.goto("https://playwright.dev/");
+    const gitHubRepository = page.locator('[aria-label="GitHub repository"]');
 
-test("Search works and finds the necessary element", async ({ page }) => {
-  await page.goto("https://playwright.dev/");
-  await page.locator(".DocSearch-Button-Placeholder").click();
-  await page.locator("#docsearch-input").fill("get");
-  await page.locator("#docsearch-item-4").click();
-  await page.locator('[aria-label="Direct link to getByLabel"]').click();
-  await expect(
-    page.locator('[aria-label="Direct link to getByLabel"]')
-  ).toBeVisible();
+    await gitHubRepository.click();
+
+    const [newPage] = await Promise.all([
+      page.waitForEvent("popup"),
+      gitHubRepository.click(),
+    ]);
+
+    await newPage.waitForLoadState();
+    await expect(
+      newPage.locator("text=microsoft / playwright Public")
+    ).toBeVisible();
+  });
+
+  test("Search works and finds the necessary element", async ({ page }) => {
+    await page.goto("https://playwright.dev/");
+    const searchButton = page.locator(".DocSearch-Button-Placeholder");
+    const docSearch = page.locator("#docsearch-input");
+    const directLink = page.locator('[aria-label="Direct link to getByLabel"]');
+    await searchButton.click();
+    await docSearch.fill("get");
+    await page.locator("#docsearch-item-4").click();
+    await directLink.click();
+    await expect(directLink).toBeVisible();
+  });
 });
